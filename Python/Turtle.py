6317b6da0f8d86df9a3a95f6b270610f1984f2ab
Ajwah <johnson786.kevin+git@gmail.com>
2015-06-26 16:58:04 -0400

    Improve Turtle Drawing Exercise

--- Python/Turtle.py
+++ Python/Turtle.py
@@ -1,20 +1,32 @@
 import turtle
 
-def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("red")
+window = turtle.Screen()
+window.bgcolor("red")
 
-    brad = turtle.Turtle()
-    brad.shape("circle")
-    brad.color("purple")
-    brad.speed()
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(50)
-    brad.right(90)
-    brad.forward(25)
-    brad.right(90)
-    brad.forward(12)
-    brad.right(90)
+def init_turtle(shape,color,speed):
+    t = turtle.Turtle()
+    t.shape(shape)
+    t.color(color)
+    t.speed(speed)
+    return t
 
-draw_square()
+def draw_square(ln):    
+    brad = init_turtle("circle","purple",2)
+    for i in range(0,4):
+        brad.forward(ln)
+        brad.right(90)
+
+def draw_triangle(ln):
+    genie = init_turtle("classic","green",4)
+    for i in range(1,4):
+        genie.forward(ln)
+        genie.right(120)
+
+
+def draw_circle(r):
+    jamie = init_turtle("square","blue",6)
+    jamie.circle(r)
+    
+draw_square(50)
+draw_triangle(100)
+draw_circle(150)
